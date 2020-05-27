ead7254607d81b2d7e52cf1baf9c8539cfd68c98
kuroringo90 <57499655+kuroringo90@users.noreply.github.com>
2020-05-27 10:09:54 +0200

    Modifica bot

--- execute.php
+++ execute.php
@@ -45,7 +45,7 @@ if(isset($message['text']))
 	
   if(strpos($text, "/start") === 0 )
   {
-	$response = "Ciao $firstname! \nMandami un link Amazon o condividilo direttamente con me da altre app! \nTi rispondero' con il link affiliato del mio padrone! Grazie mille!\n\nCreated by http://www.webemento.com";
+	$response = "Ciao $firstname! \nMandami un link Amazon o condividilo direttamente con me da altre app! \n";
   }
   elseif($dominioAmazon == "amazon")
   {	  
